8a235609572d0204dc5914c815b42d3a0eeeba8b
Sofia Saraiva <112433998+Sofia-Saraiva@users.noreply.github.com>
2023-08-24 12:10:08 -0300

    Add files via upload

---
+++ q4_spscl.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  int senha;
+
+  do {
+    scanf("%d", &senha);
+    if (senha != 2002) {
+      printf("Senha Invalida\n");
+    }
+  }
+  while (senha != 2002);
+
+  printf("Acesso Permitido\n");
+  
+  return 0;
+}
